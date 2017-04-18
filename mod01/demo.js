d3e35bfa88587c9ed00bd20980f4ac56db96a332
Maurice de Beijer <maurice.de.beijer@gmail.com>
2017-04-18 13:00:57 +0200

    TYpeScript intro

--- mod01/demo.js
+++ mod01/demo.js
@@ -1,25 +1,43 @@
-class Calculator {
-  add(x, y) {
-    return x + y;
-  }
-
-  subtract(x, y) {
-    return x - y;
-  }
-}
-
+var Calculator = (function () {
+    function Calculator() {
+    }
+    Calculator.prototype.add = function (x, y) {
+        return x + y;
+    };
+    Calculator.prototype.subtract = function (x, y) {
+        return x - y;
+    };
+    return Calculator;
+}());
 var calculator = new Calculator();
-
 var x = document.getElementById("x");
 var y = document.getElementById("y");
 var result = document.getElementById("result");
-
-document.getElementById("add").addEventListener("click", () => {
-  var sum = calculator.add(x.value, y.value);
-  result.innerText = sum.toString();
+document.getElementById("add").addEventListener("click", function () {
+    var sum = calculator.add(+x.value, +y.value);
+    result.innerText = sum.toString();
 });
-
-document.getElementById("subtract").addEventListener("click", () => {
-  var difference = calculator.subtract(x.value, y.value);
-  result.innerText = difference.toString();
+document.getElementById("subtract").addEventListener("click", function () {
+    var difference = calculator.subtract(+x.value, +y.value);
+    result.innerText = difference.toString();
 });
+var Cat = (function () {
+    function Cat() {
+    }
+    Cat.prototype.sleep = function () {
+    };
+    Cat.prototype.eat = function () { };
+    return Cat;
+}());
+var Dog = (function () {
+    function Dog() {
+    }
+    Dog.prototype.sleep = function () {
+    };
+    return Dog;
+}());
+function gotosleep(cat) {
+    cat.sleep();
+}
+gotosleep(new Cat());
+gotosleep(new Dog());
