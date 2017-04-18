fc4d4e24aa9e2131600652c80e00ed882decc819
Maurice de Beijer <maurice.de.beijer@gmail.com>
2017-04-18 15:05:59 +0200

    Advanced-TypeScript

--- mod02/demo.ts
+++ mod02/demo.ts
@@ -12,12 +12,14 @@ var calculator = new Calculator();
 
 var x = <HTMLInputElement>document.getElementById("x");
 var y = <HTMLInputElement>document.getElementById("y");
-var result = document.getElementById("result");
-
-document.getElementById("add").addEventListener("click", () => {
-  var sum = calculator.add(+x.value, +y.value);
-  result.innerText = sum.toString();
-});
+var result = document.getElementById("result") || {innerText:''};
+const add = document.getElementById("add");
+if (add) {
+  add.addEventListener("click", () => {
+    var sum = calculator.add(+x.value, +y.value);
+    result.innerText = sum.toString();
+  });
+}
 
 document.getElementById("subtract").addEventListener("click", () => {
   var difference = calculator.subtract(+x.value, +y.value);
@@ -27,24 +29,89 @@ document.getElementById("subtract").addEventListener("click", () => {
 
 
 class Cat {
+  type: "cat";
+
   sleep() {
 
   }
-  eat(){}
+  eat() { }
 }
 
+function eatStuff(options) {
+  return function (construcor) {
+    construcor.bla = options.value;
+    return construcor;
+  }
+}
 
+@function (c) { return c }
+@eatStuff({ value: 1 })
 class Dog {
-  sleep() {
-    
-  }
 
+
+  type: "dog";
+
+  sleep() {
+
+  }
+  attack() {
+
+  }
 }
 
 
-function gotosleep(cat: {sleep()}){
+function gotosleep(cat: { sleep() }) {
   cat.sleep();
 }
 
 gotosleep(new Cat())
 gotosleep(new Dog())
+gotosleep(null)
+
+
+type Pet = Cat | Dog;
+
+function eat(pet: Pet) {
+
+  switch (pet.type) {
+    case "dog":
+
+    // break;
+    // case "cat":
+
+  }
+
+  if (pet instanceof Cat) {
+
+  }
+
+
+}
+
+
+
+
+function getSomeData() {
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => reject(1000), 5000);
+  })
+}
+
+// getSomeData()
+//   .then(data => console.log(data));
+
+
+
+async function printData() {
+  try {
+    var data = await getSomeData();
+    console.log(data)
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+
+printData();
+
